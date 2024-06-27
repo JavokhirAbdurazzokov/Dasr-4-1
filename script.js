52fa6e64dda75adae1e3dca2e81c5b0b7c63c281
JavokhirAbdurazzokov <168398916+JavokhirAbdurazzokov@users.noreply.github.com>
2024-06-27 21:16:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let ism=prompt("Ismingizni kiriting")
+let familya=prompt("Familyagizni kiriting")
+let yosh=prompt("Yoshizni kiriting")
+let Yortiganfaniz =prompt("Yoqtirgan fanizni kiriting")
+let Yashashjoyi=prompt(" Yashash joyizni kiriting")
+
+console.log("Siznini ismingiz: "+ism);
+console.log( "Siznini familyangiz: " + familya);
+console.log( "Siznini Yoshizni: " + yosh);
+console.log( "Siznini Yoqtirgan fanizni: " + Yortiganfaniz);
+console.log( "Siznini Yashash joyizni: " + Yashashjoyi);
